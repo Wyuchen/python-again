8e7e18401862e6280d1d9ff32fc779b94c33fa65
wz <921588587@qq.com>
2019-12-18 23:08:41 +0800

    day2-config-add

--- day2/edit-config.py
+++ day2/edit-config.py
@@ -23,19 +23,34 @@ for line in file_search:
 '''
 
 #add_contant=evel(input("please input add contant:").strip())
+'''
+############add_contest
 file_add=open('config','r+')
 arg = {
-            'bakend': 'www.oldboy.org',
+            'bakend': 'www.taobao.org',
             'record':{
-                'server': '100.1.7.9',
-                'weight': 20,
-                'maxconn': 30
+                'server': '100.1.7.3',
+                'weight': 10,
+                'maxconn': 20
             }
         }
-
-print(str(arg))
-
+add_contant=arg['bakend'].strip()
+for line in file_add.readlines():
+    line=line.strip()
+    if add_contant  in line:
+        print(arg['bakend'].strip(), 'is exist!!!')
+        break
+else:
+    file_add.write('\n')
+    file_add.write('bakend  ')
+    file_add.write(str(arg['bakend']))
+    file_add.write('\n       ')
+    for k, v in arg['record'].items():
+        file_add.write(k.rjust(len(k) + 1, ' '))
+        file_add.write(str(arg['record'][k]).rjust(len(str(arg['record'][k])) + 1, ' '))
+    file_add.close()
 '''
+
 while True:
     choose=input("input operation num(1.查看,2.增加，3.删除):")
     file_search = open('config', 'r+')
@@ -51,10 +66,27 @@ while True:
                          break
         file_search.close()
     elif choose=='2':
-        print(2)
+        arg=eval(input('please input add content:'))
+        add_contant = arg['bakend'].strip()
+        file_search.seek(0)
+        #print(file_search.tell())
+        for line in file_search:
+            line = line.strip()
+            if add_contant in line:
+                print(arg['bakend'].strip(), 'is exist!!!')
+                break
+        else:
+            file_search.write('\n')
+            file_search.write('backend  ')
+            file_search.write(str(arg['backend']))
+            file_search.write('\n       ')
+            for k, v in arg['record'].items():
+                file_search.write(k.rjust(len(k) + 1, ' '))
+                file_search.write(str(arg['record'][k]).rjust(len(str(arg['record'][k])) + 1, ' '))
+            print('add_content success!!!')
+        file_search.close()
     elif choose=='3':
         print(3)
     else:
         print('input error!!!!')
         print('please input again:',choose)
-'''
