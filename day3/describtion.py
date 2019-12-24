dc72408b19a6bf51d0f9b387f5a157bf7297aaf3
wyc <921588587@qq.com>
2019-12-24 18:13:29 +0800

    describe

--- day3/describtion.py
+++ day3/describtion.py
@@ -2,6 +2,21 @@
 
 ''''
 装饰器:为其他函数添加其他功能
-装饰器需要注意的事项：
-1.不能修改被装饰的函数的源代码和调用方法
+1.装饰器需要注意的事项：不能修改被装饰的函数的源代码和调用方法
+2.装饰器=>高级函数和嵌套函数
+         高阶函数：把一个函数名当做实参传给另外一个函数，返回值中包含函数名
 '''
+
+import time
+def function():
+    time.sleep(2)
+    print('function running')
+
+def fun(f):
+    start_time=time.time()
+    f()
+    stop_time=time.time()
+    print("function run time: %s" %(stop_time-start_time))
+
+fun(function)
+
